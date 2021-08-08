22b039bc5ea29c81e70dbdf5dab552a5b235abf6
Crawford Vining <crawfordvining@fm-cravin2-mac.home>
2021-08-08 10:20:47 -0700

    one more file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Simple from './Simple';
 import Another from './Another';
+import Onemore from './Onemore';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
      <h1>News app built with React Hooks will go here!</h1>
      <Simple />
      <Another />
+     <Onemore />
     </div>
   );
 }
---
+++ src/Onemore.js
@@ -0,0 +1,7 @@
+import react from 'react'
+
+export default function Onemore(){
+    return(<p>
+        this is one more component!
+    </p>)
+}
