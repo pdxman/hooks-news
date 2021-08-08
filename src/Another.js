643aaa1db484da4368b7125e8f9a9cd4e34de8db
Crawford Vining <crawfordvining@fm-cravin2-mac.home>
2021-08-07 17:23:17 -0700

    new component

---
+++ src/Another.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Another(){
+
+    return(
+        <p>this is another try</p>
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Simple from './Simple';
+import Another from './Another';
 
 function App() {
   return (
     <div className="App">
      <h1>News app built with React Hooks will go here!</h1>
      <Simple />
+     <Another />
     </div>
   );
 }
