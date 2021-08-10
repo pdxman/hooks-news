5dcc56db69bf65ac9f897ff927ffd054ca781119
Crawford Vining <crawfordvining@fm-cravin2-mac.home>
2021-08-09 18:44:44 -0700

    added james bond component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import Simple from './Simple';
 import Another from './Another';
 import Onemore from './Onemore';
+import Someprops from './Someprops';
+import Goldengun from './Goldengun';
 
 function App() {
+  
   return (
     <div className="App">
      <h1>News app built with React Hooks will go here!</h1>
      <Simple />
      <Another />
      <Onemore />
+     <Someprops name="craw"/>
+     <Goldengun weapon="golden" />
     </div>
   );
 }
---
+++ src/Goldengun.js
@@ -0,0 +1,7 @@
+import React, {useState} from 'react'
+
+export default function Goldengun(props){
+    return(
+        <p>James Bond's most fearsome enemy is the man with the {props.weapon} gun</p>
+    )
+}
